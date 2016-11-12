4463b1639cfcd195ec9ca0f53a3f35f54c390196
EmixMaxime <emixmaxime@outlook.fr>
2016-11-12 16:27:00 +0100

    Improve databaseSeeder, manual code

--- test/seedDatabase.js
+++ test/seedDatabase.js
@@ -2,14 +2,49 @@ const Faker = require('Faker')
 const pg = require('../src/pg')
 const SQL = require('sql-template-strings')
 
-const fakeFilm = {
-    title: Faker.Lorem.sentence(),
-    description: Faker.Lorem.sentence()  + Faker.Lorem.sentence(),
-    date_release: "2016-11-08"
-    // Faker.Date.past()
+function Film () {
+	this.date = new Date(),
+	this.build = ['future', 'past']
 }
 
-pg.query(SQL`
-    INSERT INTO film(title, description, date_release)
-     values(${fakeFilm.title}, ${fakeFilm.description}, ${fakeFilm.date_release})`)
-console.log("ENVIRONEMENT MODE", process.env.NODE_ENV )
+Film.prototype.nowDate = function () {
+	return this.date.toLocaleDateString()
+}
+
+Film.prototype.future = function () {
+	const date = new Date()
+	date.setDate(this.date.getDate() + 2)
+	return { date_release: date.toLocaleDateString() }
+}
+
+Film.prototype.past = function () {
+	const date = new Date()
+	date.setDate(this.date.getDate() - 2)
+	return { date_release: date.toLocaleDateString() }
+}
+
+Film.prototype.base = function () {
+	return {
+    	title: Faker.Lorem.sentence(),
+    	description: Faker.Lorem.sentence()  + Faker.Lorem.sentence()
+	}
+}
+
+const film = new Film()
+let filmData = []
+
+film.build.forEach(build => {
+	const changing = film[build]()
+	const base = film.base()
+	const object = Object.assign({}, base, changing)
+	filmData.push(object)
+})
+
+filmData.forEach(data => {
+	pg.query(SQL`
+		INSERT INTO film (title, description, date_release)
+		values(${data.title}, ${data.description}, ${data.date_release})	
+	`)
+})
+
+
